da19db346f022fc19291bea07ab71ad700201168
RJJohnson162 <91172411+RJJohnson162@users.noreply.github.com>
2023-09-26 01:52:31 +0300

    reset

--- 0x00-ES6_basic/2-arrow.js
+++
@@ -1,9 +0,0 @@
-export default function getNeighborhoodsList() {
-    this.sanFranciscoNeighborhoods = ["SOMA", "Union Square"];
-
-    this.addNeighborhood = (newNeighborhood) => {
-        // Arrow function
-        this.sanFranciscoNeighborhoods.push(newNeighborhood);
-        return this.sanFranciscoNeighborhoods;
-    };
-}
